74415a746e603778144363473c186e742445db56
Coder2195 <coder2195mail@gmail.com>
2023-03-02 15:51:12 -0500

    ccc.nvim added

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@
 -- normal format is "key = value". These also handle array like data structures
 -- where a value with no key simply has an implicit numeric key
 local config = {
+    icons = { VimIcon = "", ScrollText = "" },
     -- Configure AstroNvim updates
     updater = {
         remote = "origin", -- remote to use
@@ -293,7 +294,7 @@ local config = {
                     "░ ░          ░ ░     ░       ░  ░   ░           ░   ░         ░   ",
                     "░                  ░                           ░                  "
                 }
-                local button = require("core.utils").alpha_button
+                local button = require("astronvim.utils").alpha_button
                 -- add a button to update plugins
 
                 opts.section.buttons.val = {
@@ -307,6 +308,7 @@ local config = {
 
                     button("LDR q", "  Quit Neovim")
                 }
+
                 return opts
             end
         }, {
@@ -453,27 +455,145 @@ local config = {
         }, {
         "rebelot/heirline.nvim",
         opts = function(_, opts)
-            local status = require("core.utils.status")
+            local utils = require("astronvim.utils")
+            local status = require("astronvim.utils.status")
             opts.statusline = {
                 -- statusline
                 hl = { fg = "fg", bg = "bg" },
                 status.component.mode {
-                    mode_text = { padding = { left = 1, right = 1 } }
-                }, -- add the mode text
-                status.component.git_branch(),
-                status.component.file_info {
-                    filetype = {},
-                    filename = false,
-                    file_modified = false
+                    -- enable mode text with padding as well as an icon before it
+                    mode_text = {
+                        icon = {
+                            kind = "VimIcon",
+                            padding = { right = 1, left = 1 }
+                        }
+                    },
+                    -- surround the component with a separators
+                    surround = {
+                        -- it's a left element, so use the left separator
+                        separator = "left",
+                        -- set the color of the surrounding based on the current mode using status.module
+                        color = function()
+                            return {
+                                main = status.hl.mode_bg(),
+                                right = status.hl.mode_bg()
+                            }
+                        end
+                    }
                 },
-                status.component.git_diff(),
-                status.component.diagnostics(),
+                -- we want an empty space here so we can use the component builder to make a new section with just an empty string
+                -- add a section for the currently opened file information
+                status.component.file_info {
+                    -- enable the file_icon and disable the highlighting based on filetype
+                    file_icon = { padding = { left = 1 } },
+                    filename = { fallback = "Empty" },
+                    -- add padding
+                    padding = { right = 1 },
+                    -- define the section separator
+                    surround = { separator = "left", condition = false }
+                },
+                -- add a component for the current git branch if it exists and use no separator for the sections
+                status.component.git_branch {
+                    surround = { separator = "none" }
+                },
+                -- add a component for the current git diff if it exists and use no separator for the sections
+                status.component.git_diff {
+                    padding = { left = 1 },
+                    surround = { separator = "none" }
+                },
+                -- fill the rest of the statusline
+                -- the elements after this will appear in the middle of the statusline
                 status.component.fill(),
-                status.component.cmd_info(),
+                -- add a component to display if the LSP is loading, disable showing running client names, and use no separator
+                status.component.lsp {
+                    lsp_client_names = false,
+                    surround = { separator = "none", color = "bg" }
+                },
+                -- fill the rest of the statusline
+                -- the elements after this will appear on the right of the statusline
                 status.component.fill(),
-                status.component.lsp(),
-                status.component.treesitter(),
-                status.component.nav()
+                -- add a component for the current diagnostics if it exists and use the right separator for the section
+                status.component.diagnostics {
+                    surround = { separator = "right" }
+                },
+                -- add a component to display LSP clients, disable showing LSP progress, and use the right separator
+                status.component.lsp {
+                    lsp_progress = false,
+                    surround = { separator = "right" }
+                },
+                -- NvChad has some nice icons to go along with information, so we can create a parent component to do this
+                -- all of the children of this table will be treated together as a single component
+                {
+                    -- define a simple component where the provider is just a folder icon
+                    status.component.builder {
+                        -- utils.get_icon gets the user interface icon for a closed folder with a space after it
+                        { provider = utils.get_icon "FolderClosed" },
+                        -- add padding after icon
+                        padding = { right = 1 },
+                        -- set the foreground color to be used for the icon
+                        hl = { fg = "bg" },
+                        -- use the right separator and define the background color
+                        surround = {
+                            separator = "right",
+                            color = "folder_icon_bg"
+                        }
+                    },
+                    -- add a file information component and only show the current working directory name
+                    status.component.file_info {
+                        -- we only want filename to be used and we can change the fname
+                        -- function to get the current working directory name
+                        filename = {
+                            fname = function(nr)
+                                return vim.fn.getcwd(nr)
+                            end,
+                            padding = { left = 1 }
+                        },
+                        -- disable all other elements of the file_info component
+                        file_icon = false,
+                        file_modified = false,
+                        file_read_only = false,
+                        -- use no separator for this part but define a background color
+                        surround = {
+                            separator = "none",
+                            color = "file_info_bg",
+                            condition = false
+                        }
+                    }
+                },
+                -- the final component of the NvChad statusline is the navigation section
+                -- this is very similar to the previous current working directory section with the icon
+                { -- make nav section with icon border
+                    -- define a custom component with just a file icon
+                    status.component.builder {
+                        { provider = utils.get_icon "ScrollText" },
+                        -- add padding after icon
+                        padding = { right = 1 },
+                        -- set the icon foreground
+                        hl = { fg = "bg" },
+                        -- use the right separator and define the background color
+                        -- as well as the color to the left of the separator
+                        surround = {
+                            separator = "right",
+                            color = {
+                                main = "nav_icon_bg",
+                                left = "file_info_bg"
+                            }
+                        }
+                    },
+                    -- add a navigation component and just display the percentage of progress in the file
+                    status.component.nav {
+                        -- add some padding for the percentage provider
+                        percentage = { padding = { right = 1 } },
+                        -- disable all other providers
+                        ruler = false,
+                        scrollbar = false,
+                        -- use no separator and define the background color
+                        surround = {
+                            separator = "none",
+                            color = "file_info_bg"
+                        }
+                    }
+                }
                 -- remove the 2nd mode indicator on the right
             }
 
@@ -488,8 +608,7 @@ local config = {
         end,
         event = "VeryLazy",
         dependencies = { "nvim-telescope/telescope.nvim" }
-    }, { "seandewar/nvimesweeper", event = "VeryLazy" },
-        { "stsewd/spotify.nvim",    event = "VeryLazy" }, {
+    }, { "seandewar/nvimesweeper", event = "VeryLazy" }, {
         "simrat39/rust-tools.nvim",
         config = function()
             local rt = require("rust-tools")
@@ -537,11 +656,41 @@ local config = {
         end,
         event = "VeryLazy",
         dependencies = { "neovim/nvim-lspconfig" }
-    }
+    }, {
+        "uga-rosa/ccc.nvim",
+        event = "VeryLazy",
+        config = function()
+            vim.opt.termguicolors = true
 
+            local ccc = require("ccc")
+            local mapping = ccc.mapping
+            ccc.setup({ highlighter = { auto_enable = true } })
+        end
+    }
     },
     -- Customize Heirline options
     heirline = {
+        separators = {
+            left = { "", " " }, -- separator for the left side of the statusline
+            right = { " ", "" } -- separator for the right side of the statusline
+        },
+        attributes = { mode = { bold = true } },
+        colors = function(hl)
+            -- use helper function to get highlight group properties
+            local utils = require("astronvim.utils")
+            local comment_fg = utils.get_hlgroup("Comment").fg
+            hl.git_branch_fg = comment_fg
+            hl.git_added = comment_fg
+            hl.git_changed = comment_fg
+            hl.git_removed = comment_fg
+            hl.blank_bg = utils.get_hlgroup("Folded").fg
+            hl.file_info_bg = utils.get_hlgroup("Visual").bg
+            hl.nav_icon_bg = utils.get_hlgroup("String").fg
+            hl.nav_fg = hl.nav_icon_bg
+            hl.folder_icon_bg = utils.get_hlgroup("Error").fg
+            return hl
+        end,
+        icon_highlights = { file_icon = { statusline = false } }
         -- -- Customize different separators between sections
         -- separators = {
         --   breadcrumbs = " > ",
